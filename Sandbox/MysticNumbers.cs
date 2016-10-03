329e8a14e219eec3f53c9ba71c2cb139de2a825d
Martin Kierkegaard <mark@easj.dk>
2016-10-03 21:16:56 +0200

    Add project files.

---
+++ Sandbox/MysticNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class MysticNumbers
+    {
+        public int ThreeNumbers(int a, int b, int c)
+        {
+            int result;
+
+            if (b > a)
+            {
+                result = b;
+                if (c > b)
+                {
+                    result = c;
+                }
+            }
+            else
+            {
+                result = a;
+                if (c > a)
+                {
+                    result = c;
+                }
+            }
+
+            return result;
+        }
+    }
+}
